9c70099856396b760c298ce6a7ef02a63af6b29f
lea905 <leabarnezet@gmail.com>
2025-05-31 20:03:11 +0200

    [ADD] ajout des arene en BD ainsi que sur la page d'accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controller;
+use App\Repository\AreneRepository;
 use App\Repository\DresseurRepository;
 use App\Repository\PokemonRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,14 +11,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home_index', methods: ['GET'])]
-    public function home(PokemonRepository $pokemonRepository, DresseurRepository $dresseurRepository): Response
+    public function home(PokemonRepository $pokemonRepository, DresseurRepository $dresseurRepository,
+    AreneRepository $areneRepository): Response
     {
         $pokemons = array_slice($pokemonRepository->findAll(), 0, 6);
         $dresseurs = array_slice($dresseurRepository->findAll(), 0, 6);
+        $arenes = $areneRepository->findAll();
 
         return $this->render('index.html.twig',[
             'pokemons' => $pokemons,
-            'dresseurs' => $dresseurs
+            'dresseurs' => $dresseurs,
+            'arenes' => $arenes,
         ]);
     }
 }
